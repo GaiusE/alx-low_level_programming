b40322eecb60e508e8fc398261849f48c37c6b00
gaiusE <gaiusbakure@gmail.com>
2022-12-20 14:05:44 -0800

    98 Battery st.

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,12 +1,11 @@
 void reset_to_98(int *n);
-	void swap_int(int *a, int *b);
-	int _strlen(char *s);
-	void _puts(char *str);
-	int _putchar(char *c);
-	void print_rev(char *s);
-	void put2(char *str);
-	void puts_half(char *str);
-	void print_array(int *a int *b);
-	char *_strcpy(char *dest, char *src);
-	void rev_string(char *s);
-
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+int _putchar(char *c);
+void print_rev(char *s);
+void put2(char *str);
+void puts_half(char *str);
+void print_array(int *a int *b);
+char *_strcpy(char *dest, char *src);
+void rev_string(char *s);
