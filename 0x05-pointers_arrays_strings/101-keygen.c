e6b08603b7f703d0ffb849e44470ca7f44f20813
gaiusE <gaiusbakure@gmail.com>
2022-12-20 18:06:04 -0800

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -23,9 +23,9 @@ int main(void)
 		pass[i] = rand() % 78;
 		sum += (pass[i] + '0');
 		putchar(pass[i] + '0');
-		if ((2722 - sum) - '0' < 78)
+		if ((2772 - sum) - '0' < 78)
 		{
-			n = 2722 - sum - '0';
+			n = 2772 - sum - '0';
 			sum += n;
 			putchar(n + '0');
 			break;
