905841e772a69759d9fd13f197961f758e52f7ef
gaiusE <gaiusbakure@gmail.com>
2022-12-20 18:19:41 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
 /**
- * put2 - prints one char out of 2 of a string.
+ * puts2 - prints one char out of 2 of a string.
  * @str: input string.
  *
  * Return: void.
  */
 void puts2(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (str[i] |= '\0')
+	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
+
 		i++;
 	}
 	_putchar('\n');
