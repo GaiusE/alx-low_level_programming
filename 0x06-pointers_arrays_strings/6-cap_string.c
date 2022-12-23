5b1360381533657ffec2642bb4855abfbaee3f2c
gaiusE <gaiusbakure@gmail.com>
2022-12-23 15:55:15 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,35 @@
 #include "main.h"
+
 /**
  * cap_string - function that capitalize first character of a word
  * @str: string to capitalize
  * Return: returns the capitalized string
  */
-char *cap_string(char *)
+char *cap_string(char *str)
 {
-        int index = 0;
+	int i = 0;
 
-        while (str[++index])
-        {
-                while (!(str[index] >= 'a' && str[index] <= 'z'))
-                        index++;
+	while (str[i])
+	{
+                while (!(str[i] >= 'a' && str[i] <= 'z'))
 
-                if (str[index - 1] == ' ' ||
-                                str[index - 1] == '\t' ||
-                                str[index - 1] == '\n' ||
-                                str[index - 1] == ',' ||
-                                str[index - 1] == ';' ||
-                                str[index - 1] == '.' ||
-                                str[index - 1] == '!' ||
-                                str[index - 1] == '?' ||
-                                str[index - 1] == '"' ||
-                                str[index - 1] == '(' ||
-                                str[index - 1] == ')' ||
-                                str[index - 1] == '{' ||
-                                str[index - 1] == '}' ||
-                        str[index] -= 32;
-        }
-        return (str);
+			i++;
+		if (str[i - 1] == ' ' ||
+				str[i - 1] == '\t' ||str[i - 1] == '\n' ||
+				str[i - 1] == ',' ||
+                                str[i - 1] == ';' ||
+                                str[i - 1] == '.' ||
+                                str[i - 1] == '!' ||
+                                str[i - 1] == '?' ||
+                                str[i - 1] == '"' ||
+                                str[i - 1] == '(' ||
+                                str[i - 1] == ')' ||
+                                str[i - 1] == '{' ||
+                                str[i - 1] == '}' ||
+				i == 0)
+				str[i] -= 32;
+		i++;
+	}
+
+	return (str);
 }
