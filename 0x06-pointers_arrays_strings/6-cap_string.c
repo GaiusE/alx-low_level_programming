3bca1dafc89ba9d06b18d8ff284be0f23a19dfdb
gaiusE <gaiusbakure@gmail.com>
2022-12-23 16:04:12 -0800

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,8 @@ char *cap_string(char *str)
 
 			i++;
 		if (str[i - 1] == ' ' ||
-				str[i - 1] == '\t' ||str[i - 1] == '\n' ||
+				str[i - 1] == '\t'||
+				str[i - 1] == '\n'||
 				str[i - 1] == ',' ||
                                 str[i - 1] == ';' ||
                                 str[i - 1] == '.' ||
